524a17ba2b600b801b06d320a17741e84e645daa
mashoodshaukat00 <75940719+mashoodshaukat00@users.noreply.github.com>
2021-04-16 02:15:52 +0200

    work on Navbar done.

---
+++ eesys-app/src/Nav.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Nav.css';
+function Nav() {
+    return (
+      <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+  {/* <!-- Brand/logo --> */}
+  <div className='container'>
+  <a className="navbar-brand text-success font-italic font-weight-bold" href="#">EatEat</a>
+  <input type="text" className='rounded p-1  text-center outline ' placeholder='Search' />
+  
+  {/* <!-- Links --> */}
+  <ul className="navbar-nav ">
+    <li className="nav-item ">
+      <a className="nav-link h5 text-success " href="#">Login</a>
+    </li>
+    <li className="nav-item mx-3 ">
+      <a className="nav-link h5 text-success" href="#">New User</a>
+    </li>
+    <li className="nav-item">
+      <a className="nav-link h5 text-success" href="#">
+      <div className="dropdown">
+  <a type="button" className=" bg-dark text-success dropdown-toggle" data-toggle="dropdown">
+    Norsk
+  </a>
+  <div className="dropdown-menu mt-3 bg-dark">
+    <a className="dropdown-item text-success" href="#">English</a>
+    <a className="dropdown-item text-success" href="#">Svensk</a>
+    <a className="dropdown-item text-success" href="#">Dansk</a>
+  </div>
+</div>
+      </a>
+    </li>
+  </ul>
+  </div>
+</nav>
+    )
+}
+
+export default Nav
